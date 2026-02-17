2d7e18a98063d85c4d9aaf37f7ae6b2045945d51
locnht3 <locnht3@gmail.com>
2026-02-17 22:33:08 +0700

    feat(Session03-Class): update DogManagement project

--- Session03-Class/DogManagement/src/data/Dog.java
+++ Session03-Class/DogManagement/src/data/Dog.java
@@ -7,10 +7,29 @@ public class Dog {
     
     // Phễu dùng để rót/chế/đổ/fill vật liệu đưa vào,
     // lấp đầy các chi tiết chừa sẵn chỗ. Khuôn rỗng ở các chi tiết.
+    // PHỄU LÀ MỘT HÀM RẤT ĐẶC BIỆT, nó có xử lí: ép dòng dữ liệu/vật liệu nhận từ bên ngoài,
+    // chảy vào đúng chỗ bên trong - xử lí - hàm quan điểm của lập trình.
+    // MỖI LẦN GỌI PHỄU, SỬ DỤNG PHỄU TƯƠNG ĐƯƠNG VỚI VIỆC ĐÚC 1 BỨC TƯỢNG MỚI, TẠO 1 OBJECT MỚI,
+    // GIỐNG ĐI MUA 1 EM DOG ĐEM VỀ BÁN, NUÔI (CHO TÊN, GHI CÂN NẶNG, VACCINE ...).
+    // Lưu ý hàm này tên phải trùng 100% tên class, chữ hoa từng đầu từ - quy ước.
+    // Tên hàm phễu KHÔNG CÓ GIÁ TRỊ TRẢ VỀ, KHÔNG ĐƯỢC CÓ VOID vì void sẽ nhầm lẫn với các ứng xử/hành xử của object.
+    // Hàm phễu nhận vật liệu vào chờ khô và ra được chính object thuộc nhóm Dog đang đề cập,
+    // trả về ngay object sẽ được tạo trong tương lai, đúng không gian mình đang đứng đã là object,
+    // trong khi đó nhờ phễu được object này.
     public Dog(String iName, int iYob, double iWeight) {
         name = iName;
         yob = iYob;
         weight = iWeight;
+    }
+    
+    // sủa() là hành động của object Dog.
+    // speak() là hành động của Person.
+    // scanChanel(), showMovie() là hành động của TiVi.
+    
+    public void bark() {
+//        System.out.println("gogogo... my name is " + name);
+//        System.out.println("gogogo... my yob is " + yob);
+        System.out.printf("|GOGO|%-10s|%4d|%4.1f|\n", name, yob, weight);
     }
     
 }
@@ -22,3 +41,5 @@ public class Dog {
 // CCCD cũng là Khuôn để chứa các info mô tả công dân.
 // CaSi là Khuôn chứa các info mô tả mọi object đi hát.
 // Khuôn thì phải kín, code phải viết trong class.
+// KHUÔN LÀ HÌNH DÁNG CỦA 1 OBJECT SẼ XUẤT HIỆN TRONG TƯƠNG LAI,
+// CHỨA ĐỰNG ĐẶC TÍNH/ĐẶC ĐIỂM VÀ HÀNH VI (XỬ LÍ INFO - HÀM).
--- Session03-Class/DogManagement/src/dogmanagement/DogManagement.java
+++ Session03-Class/DogManagement/src/dogmanagement/DogManagement.java
@@ -1,9 +1,26 @@
 package dogmanagement;
 
+import data.Dog;
+// Đi mượn cái Khuôn Dog ở ngăn data về đúc.
+// ~ #include<stdio.h> bên C.
+
 public class DogManagement {
 
     public static void main(String[] args) {
+        int             yob         =       2003;
+    // data type    tên gọi/biến        value, single, đơn, primitive
         
+        Dog             chiHu       =       new Dog("CHI HU HU", 2026, 0.5);        
+    // data type   tên biến object              phức tạp, object
+        
+        chiHu.bark();
+        
+        Dog ngaoDa = new Dog("NGÁO ĐÁ", 2020, 50.0);
+        ngaoDa.bark();
+        
+        // Kiểm tra xem khi đổ vào Khuôn, đứa sau có đè đứa trước hay không?
+        System.out.println("chiHu barks again.");
+        chiHu.bark();
     }
     
 }

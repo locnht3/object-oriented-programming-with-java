d682ae3b2bf9c8339cca61fae56057e679c694f4
locnht3 <locnht3@gmail.com>
2026-02-17 11:29:38 +0700

    feat(Session01-Hello): add HelloJava project

---
+++ Session01-Hello/HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,14 @@
+package hellojava;
+
+public class HelloJava {
+    
+    // Entry point, cửa chính, CPU vào đây chạy lệnh.
+    public static void main(String[] args) {
+        System.out.println("Hello Java!\nXin chào Java!");
+        System.out.println("From Ngo Huynh Tan Loc <3");
+        
+        System.out.println("5 + 5 = " + 5 + 5);     // = 55
+        System.out.println("5 + 5 = " + (5+ 5));    // = 10
+    }
+    
+}

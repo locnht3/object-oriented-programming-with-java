fe93a61290fbb7a76cf61bb47273ae8549500336
locnht3 <locnht3@gmail.com>
2026-02-20 23:05:20 +0700

    feat(Session05-Array): update StudentManagement project

--- Session05-Array/StudentManagement/src/data/Shelf.java
+++ Session05-Array/StudentManagement/src/data/Shelf.java
@@ -12,20 +12,33 @@ public class Shelf {
     // OOP: tìm các object, chúng phối hợp, trộn nhau
     private String color;
     private String label; // tủ chén, tủ hồ sơ SE, IA, GD
+    
+    // biến được đưa value lúc nào? 
+    //                              qua phễu (default trong suy nghĩ)
+    //                              gán trực tiếp lúc khai báo
 
-    // chứa cái gì
+    // chứa cái gì, chừa không gian để chứa đồ, thực ra đồ chưa có
+    //              chưa có hồ sơ sinh viên
     private Student[] ds = new Student[300];
+    //      ds là biến má mì - cần value
+    //                     new sẵn 300 tương đương mua tủ 300 lít
+    //                     đặt hàng kích thước, dung tích size 500, 1000, 2000
+    
     private Scanner sc = new Scanner(System.in);
     private int count = 0;  // mới mua tủ về, số hồ sơ = 0
                             // nhét thêm 1 hồ sơ thì count++
                             // chơi mảng chỉ for đến count
     
     // Tôi có không gian chứa info, tôi sẽ giải quyết chuyện nhập xuất
-    public Shelf(String color, String label) {
+    public Shelf(String color, String label) { // int size - kích thước tủ
         this.color = color;
         this.label = label;
+        //ds = new Student[size]; // linh hoạt trong kích thước tủ
     }
-
+    
+    // DATA + HÀM XỬ LÍ CỦA TỦ NHÂN CÁCH HÓA, TOÀN BỘ LÀ NON-STATIC
+    // NẾU KHÔNG THÌ CÁC TRƯỜNG ĐẠI HỌC, BỘ MÔN XÀI CHUNG DSSV THÌ TOANG
+    // ĐẶC ĐIỂM, HÀNH XỬ PHẢI THUỘC VỀ OBJECT
     public void inputAStudent() {
         String id, name;
         int yob;
@@ -45,14 +58,38 @@ public class Shelf {
         gpa = Double.parseDouble(sc.nextLine());
 
         ds[count] = new Student(id, name, yob, gpa);
-        count++; 
+        count++;
+        System.out.println("Add student successfully");
     }
     
     public void printStudentList() {
         System.out.println("The student list");
         for (int i = 0; i < count; i++) {
             ds[i].showProfile();    // FOR HẾT LÀ TOANG!!!
+        }                           // VÌ MẸ KIẾP, MẢNG OBJECT DEFAULT CÁC PHẦN TỬ LÀ NULL
+    }                               // HẠ CÁNH NƠI ANH, TRỎ ĐÁY RAM.
+    
+    public void searchAStudent() {
+        // search là quét/duyệt/đi từ trái sang phải lôi cổ từng sv[i] ra
+        // hỏi hắn mã số là mấy?
+        // so sánh với mã số muốn tìm? == mã số of sv[i] sv[i].getMS() -> báo tìm thấy
+        // tìm hết không thấy thì báo rằng not found
+        // THUẬT TOÁN TRÂU BÒ/VÉT CẠN/TÁT CẠN
+        System.out.print("Input the id that you want to search: ");
+        String id = sc.nextLine(); // hoa thường à nhen
+        for (int i = 0; i < count; i++) {
+            if (ds[i].getId().equalsIgnoreCase(id)) {
+                // tìm thấy tại vị trí [i]
+                System.out.println("Student found!!! Here she/he is");
+                ds[i].showProfile(); // in kết quả liền
+                return;
+            }   // tìm thấy thì dừng hàm luôn, không for gì nữa
         }
+        
+        // Sống sót tới đây, sure, không có thấy id cần tìm, nếu thấy đã dừng sớm ở trên,
+        // đi hết for mà không thoát được, báo không thấy
+        System.out.println("Student not found!!!");
+        // dùng biến flag phất cờ nếu thấy và không thấy, if () in ra câu thông báo
     }
     
 }
--- Session05-Array/StudentManagement/src/main/Stage.java
+++ Session05-Array/StudentManagement/src/main/Stage.java
@@ -6,51 +6,110 @@ import java.util.Scanner;
 
 public class Stage {
 
-    public static void main(String[] args) {      
+    public static void main(String[] args) {
+        // Chơi menu: 1. Add ...; 2. Print ...; 3. Search ...; 4. Quit
+        // Chờ người chơi chọn món 1 2 3 4 Ahihi - try-catch-do-while
+        // if chọn 1 thì tu.add() 2 thì tu.print() 3 thì ...
+        // chưa xỉn chưa về, chưa muốn dừng cuộc chơi thì không cho thoát - do-while/while
+        Shelf tuSE = new Shelf("PINK", "SE");
+        Scanner sc = new Scanner(System.in);
+        int choice; // lưu option lựa chọn menu gõ từ bàn phím
+
+        do {
+            printMenu();
+            System.out.print("Input your choice(1..6): ");
+            choice = Integer.parseInt(sc.nextLine());
+
+            //if (choice == 1) == 2 == 3 == 4 == 5
+            switch (choice) {
+                case 1:
+                    tuSE.inputAStudent();
+                    break;
+                case 2:
+                    tuSE.printStudentList();
+                    break;
+                case 3:
+                    tuSE.searchAStudent();
+                    break;
+                case 6:
+                    System.out.println("Bye bye, see you next time!");
+                    break;
+                default:
+                    System.out.println("Please choose 1 to 6, please!!!");
+                    break;
+            }
+        } while (choice != 6); // còn chưa chọn 6 còn lặp lại.
+                        // còn đúng thì còn do-làm
+                        // đúng là chưa == 6 thì cứ làm đi
+                        // chưa bằng 6 đúng ko? đúng hả, làm tiếp đi
+
+    }
+
+    public static void printMenu() {
+        System.out.println("Welcome to FAP - FPT Academic Portal");
+        System.out.println("Choose the following functions to play with");
+        System.out.println("1. Add a student profile");
+        System.out.println("2. Print student list");
+        System.out.println("3. Search a student by id");
+        System.out.println("4. Update a student profile");
+        System.out.println("5. Remove a student");  // REMOVE HẾT SỨC CẨN THẬN
+        // XÓA KHÔNG HẲN LÀ XÓA, MÀ XÓA LÀ ẨN
+        // CẤT CHỖ KHÁC DÀNH CHO NHU CẦU THỐNG KÊ
+
+        System.out.println("6. Quit");
+    }
+
+    public static void testTu() {
         //inputStudentList();
         // NGON THÌ MENU VÒNG LẶP CHỌN 1 THÊM HỒ SƠ, 2 IN DANH SÁCH
-        
+
         Shelf tuSE = new Shelf("PINK", "SE");
-        
+
+        Shelf tuGD = new Shelf("RAINBOW", "GD"); // 2 cái tủ độc lập
+        // 2 tủ 2 góc phòng
+
         tuSE.inputAStudent();
         tuSE.inputAStudent();
-        
-        tuSE.printStudentList();
+
+        tuGD.inputAStudent();
+
+        tuSE.printStudentList(); // 2 bạn sinh viên
+        tuGD.printStudentList(); // 1 bạn sinh viên
     }
-    
+
     public static void inputStudentList() {
         Scanner sc = new Scanner(System.in);
         String id, name;
         int yob, count;
         double gpa;
-        
+
         System.out.print("How many students do you want to input? ");
         count = Integer.parseInt(sc.nextLine());
-        
+
         Student[] arr = new Student[count]; // count = 5
-        
+
         // for nhập 5 hồ sơ
         for (int i = 0; i < count; i++) {
             System.out.println("Input student #" + (i + 1) + "/" + count);
             System.out.print("Input id: ");
             id = sc.nextLine();
-            
+
             System.out.print("Input name: ");
             name = sc.nextLine();
-            
+
             System.out.print("Input yob: ");
             yob = Integer.parseInt(sc.nextLine());
-            
+
             System.out.print("Input gpa: ");
             gpa = Double.parseDouble(sc.nextLine());
-        
+
             arr[i] = new Student(id, name, yob, gpa);
         }
-        
+
         System.out.println("The student list");
         for (Student x : arr) {
             x.showProfile();
         }
     }
-    
+
 }
